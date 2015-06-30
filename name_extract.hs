2ece89f8c36f0db7f0a7ca5cb8279daa3ada2e05
fkirsek <fkirsek@gmail.com>
2015-06-30 14:05:07 +0200

    More awk files, more results

--- name_extract.hs
+++
@@ -1,9 +0,0 @@
-
-get_line_begginings :: String -> String
-get_line_begginings = unlines . map head . map (wordsWhen (==':')) . lines 
-
-wordsWhen     :: (Char -> Bool) -> String -> [String]
-wordsWhen p s =  case dropWhile p s of
-                      "" -> []
-                      s' -> w : wordsWhen p s''
-                            where (w, s'') = break p s'
